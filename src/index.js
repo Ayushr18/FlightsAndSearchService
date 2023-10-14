12d01cf161df133e2c9d4768d405e5cc5d028adb
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-15 01:07:40 +0530

    Added body-parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+
 const {PORT} = require('./config/serverConfig');
+
 const setupAndStartServer = async () => {
 
     const app = express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended: true}));
 
     app.listen(PORT, () => {
         console.log(`Server Started at ${PORT}`);
