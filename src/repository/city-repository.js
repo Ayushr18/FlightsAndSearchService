3ba94d8527ed8ff68320b66714acf218f133a84c
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-16 18:51:39 +0530

    minor fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -30,11 +30,15 @@ class CityRepository {
 
     async updateCity(cityId, data) {
         try {
-            const city = await City.update(data, {
-                where: {
-                    id: cityId
-                }
-            });
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     }
+            // });
+
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log('something went wrong in the repository layer');
