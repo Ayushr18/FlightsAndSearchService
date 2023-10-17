edf315fb6eeb7506fbdd0f3308a953f87dfd8ac1
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-17 12:23:42 +0530

    Added filters get all cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const {Op} = require('sequelize');
 const {City} = require('../models/index');
 
 class CityRepository {
@@ -56,8 +57,18 @@ class CityRepository {
         }
     }
 
-    async getAllCities() {
+    async getAllCities(filter) {
         try {
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
             const cities = await City.findAll();
             return cities;
         } catch (error) {
