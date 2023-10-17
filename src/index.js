25c533fc8832cc07e66946c5e5a510f04a9b11f0
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-17 18:06:06 +0530

    Added airports seeders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const bodyparser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
+//const {Airport, City} = require('./models/index');
+const db = require('./models/index');
+//const airport = require('./models/airport');
+
 const setupAndStartServer = async () => {
 
     const app = express();
@@ -14,7 +18,12 @@ const setupAndStartServer = async () => {
     app.use('/api', ApiRoutes);
 
     app.listen(PORT, async () => {
-        console.log(`Server Started at ${PORT}`);   
+        console.log(`Server Started at ${PORT}`);  
+        
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true}); 
+        }   
+
     });
  
 }
