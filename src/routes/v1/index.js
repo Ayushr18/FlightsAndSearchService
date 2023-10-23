6a83b018c23102ee5d2fcd74862ea78f0bbbef7a
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-23 20:07:08 +0530

    Added flights Search crud with few filters

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -11,6 +11,7 @@ router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
 
 router.post('/flights', FlightController.create);
+router.get('/flights', FlightController.getAll);
 
 module.exports = router;
  
