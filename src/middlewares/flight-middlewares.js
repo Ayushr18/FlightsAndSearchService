f1d774b28380869f3b2dc67acd5d7be3b2638b41
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-24 15:16:09 +0530

    Added error code enums

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -1,3 +1,4 @@
+const {ClientErrorCodes} = require('../utils/error-codes');
 const ValidateCreateFlight = (req, res, next) => {
     if(
         !req.body.flightNumber ||
@@ -8,7 +9,7 @@ const ValidateCreateFlight = (req, res, next) => {
         !req.body.departureTime ||
         !req.body.price
     ) {
-        return res.status(400).json({
+        return res.status(ClientErrorCodes.BAD_REQUEST).json({
             data: {},
             success: false,
             message: 'Invalid request body for create flight',
