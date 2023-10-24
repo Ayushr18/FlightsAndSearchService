4c3f91103bc4a251bd28355de33e570aeabb45f6
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-24 13:45:07 +0530

    Added common crud classes for basic crud operations

---
+++ src/repository/crud-repository.js
@@ -0,0 +1,64 @@
+class CrudRepository {
+    constructor(model) {
+        this.model= model;
+    }
+
+    async create(data) {
+        try {
+           const result = await this.model.create(data);
+           return result; 
+        } catch (error) {
+            console.log("Something went wrong in crud repo");
+            throw error;
+        }
+    }
+
+    async destroy(modelId) {
+        try {
+            await this.model.destroy({
+                where: {
+                    id: this.modelId
+                }
+            })
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in crud repo");
+            throw error;
+        }
+    }
+
+    async get(modelId) {
+        try {
+            const result = await this.findByPk(modelId);
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in crud repo");
+            throw error;
+        }
+    }
+
+    async getAll() {
+        try {
+            const result = await this.model.findAll()
+        } catch (error) {
+            console.log("Something went wrong in crud repo");
+            throw error;
+        }
+    }
+
+    async update(modelId, data) {
+        try {
+            const result = await this.model.update(data, {
+                where: {
+                    id: modelId
+                },
+            });
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in crud repo");
+            throw error;   
+        }
+    }
+}
+
+module.exports = CrudRepository;
---
+++ src/services/crud-service.js
@@ -0,0 +1,58 @@
+class CrudService {
+    constructor(repository) {
+        this.repository = repository
+    }
+
+    async create(data) {
+        try {
+          const response = await this.repository.create(data);
+          return response; 
+        } catch (error) {
+            console.log("Something went wrong in crud service");
+            throw error;
+        }
+    }
+
+    async destroy(id) {
+        try {
+           const response = await this.repository.destroy(id);
+           return response; 
+        } catch (error) {
+            console.log("Something went wrong in crud service");
+            throw error;
+        }
+    }
+
+    async get(id) {
+        try {
+            const response = await this.repository.get(id);
+           return response;  
+        } catch (error) {
+            console.log("Something went wrong in crud service");
+            throw error;
+        }
+    }
+
+    async getAll() {
+        try {
+            const response = await this.repository.getAll();
+            return response; 
+        } catch (error) {
+            console.log("Something went wrong in crud service");
+            throw error;
+        }
+    }
+
+    async update(id, data) {
+        try {
+            const response = await this.repository.update(id, data);
+            return response; 
+        } catch (error) {
+            console.log("Something went wrong in crud service");
+            throw error;
+        }
+    }
+
+}
+
+module.exports =  CrudService;
